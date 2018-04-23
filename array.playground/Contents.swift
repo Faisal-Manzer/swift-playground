b4a6799c4c1eecd944971c978049a5bdb30bf8b5
Faisal-Manzer <faisal_manzer@Faisals-MacBook.local>
2018-04-23 23:53:48 +0530

    Compleated array, one clearance needed

--- array.playground/Contents.swift
+++ array.playground/Contents.swift
@@ -13,7 +13,19 @@ var appleSoftware = [
 
 // Changing a value
 appleSoftware["iMac"] = "macOS High Sierra"
-print(appleSoftware["iMac"])
+
+//----------------------------------
+// need serious explaination in code below
+//----------------------------------
+
+//print(appleSoftware["iMac"])
+// prints Optional("macOS High Sierra")
+// gives warning
+// Expression implicitly coerced from 'String?' to Any
+
+//print(String(appleSoftware["iMac"]))
+// error: array.playground:15:7: error: cannot invoke initializer for type 'String' with an argument list of type '(String?)'
+
 //print(String(appleSoftware[2]))
 // error: array.playground:10:14: error: cannot subscript a value of type '[String : String]' with an index of type 'Int'
 
@@ -23,3 +35,11 @@ var friendList = [String]()
 // Declaring an empty dictionary Name: age
 var ageList = [String: Float]()
 
+// Empty Array
+//var anEmptyArray = []
+// Empty collection literal requires an explicit type
+
+// Clearing an array
+appleHardware = []
+
+// Clearing dictionary
